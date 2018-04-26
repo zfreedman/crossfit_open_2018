90f293fd3d3f622ba4ce4e474c36ea75fc5fcf49
zfreedman <zfreedman92@gmail.com>
2018-04-26 15:16:20 -0700

    feat: Refactored outlier filtering module and plotted first separation

--- custom_modules/outlier_filter.py
+++ custom_modules/outlier_filter.py
@@ -5,17 +5,16 @@ normal/abnormal lists.
 
 import numpy as np
 
-def separate_outliers(std_lim, vals):
+def separate_outliers(std_lim, df, col):
     """
     :param std_lim: number of standarad deviations to limit around the mean for "normal" data
         (exclusive interval)
-    :param vals: list-like values to separate
-    :return: a tuple of lists of the data as (normal, outlier-like)
+    :param df: dataframe of values to separate
+    :param col: column of data to analyze
+    :return: a tuple of dataframes as (normal, outlier-like)
     """
-    mean = np.mean(vals)
-    std = np.std(vals)
-    normal = []
-    abnormal = []
-    for i in vals:
-        normal.append(i) if np.abs(mean - i) < std_lim * std else abnormal.append(i)
+    mean = np.mean(df[col])
+    std = np.std(df[col])
+    normal = df[np.abs(df[col] - mean) < std_lim * std]
+    abnormal = df[np.abs(df[col] - mean) >= std_lim * std]
     return (normal, abnormal)
